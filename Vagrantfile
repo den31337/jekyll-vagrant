dbdfcefc47f8783dd17b02d8ebe64e28eaa0bc55
den31337 <85769665+den31337@users.noreply.github.com>
2021-06-14 02:00:57 +0300

    Add files via upload

---
+++ Vagrantfile
@@ -0,0 +1,5 @@
+Vagrant.configure("2") do |config|
+    config.vm.box = "ubuntu/bionic64"
+    config.vm.provision :shell, :path => "bootstrap.sh"
+    config.vm.network "forwarded_port", guest: 4000, host: 4000
+end
---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+apt-get -y update
+sudo apt-get -y install ruby-full build-essential zlib1g-dev
+echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc
+echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
+echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+gem install jekyll bundler
